963f978c534f80a2752e4eddc91768263b7f4f50
Colin <colin.diesh@gmail.com>
2019-01-25 11:58:59 -0500

    Add remote file handle

--- test/bigwig.test.js
+++ test/bigwig.test.js
@@ -1,6 +1,7 @@
 import BigWig from '../src/bigwig'
 
 const LocalFile = require('../src/localFile')
+const RemoteFile = require('../src/remoteFile')
 
 describe('bigwig formats', () => {
   it('loads small bigwig file', async () => {
@@ -13,5 +14,42 @@ describe('bigwig formats', () => {
     expect(feats1).toMatchSnapshot()
     expect(feats2).toMatchSnapshot()
     expect(feats3).toMatchSnapshot()
+    const feats4 = await ti.getFeatures('ctgA', 0, 1000)
+    const feats5 = await ti.getFeatures('ctgA', 2000, 2100, { scale: 0.001 })
+  })
+
+  it('loads a larger bigwig file', async () => {
+    const ti = new BigWig({
+      filehandle: new LocalFile(require.resolve('./data/cow.bw')),
+    })
+    const feats1 = await ti.getFeatures('GK000001.2', 10000, 20000, { scale: 1 })
+    console.log(feats1)
+    expect(feats1).toMatchSnapshot()
+  })
+})
+
+
+describe('bigwig formats remote', () => {
+  it('loads small bigwig file remote', async () => {
+    const ti = new BigWig({
+      filehandle: new RemoteFile('http://localhost/volvox.bw'),
+    })
+    const feats1 = await ti.getFeatures('ctgA', 0, 100, { scale: 1 })
+    const feats2 = await ti.getFeatures('ctgA', 0, 100, { scale: 0.01 })
+    const feats3 = await ti.getFeatures('ctgA', 0, 100, { scale: 0.001 })
+    expect(feats1).toMatchSnapshot()
+    expect(feats2).toMatchSnapshot()
+    expect(feats3).toMatchSnapshot()
+    const feats4 = await ti.getFeatures('ctgA', 0, 1000)
+    const feats5 = await ti.getFeatures('ctgA', 2000, 2100, { scale: 0.001 })
+  })
+
+  it('loads a larger bigwig file remote', async () => {
+    const ti = new BigWig({
+      filehandle: new RemoteFile('http://localhost/cow.bw'),
+    })
+    const feats1 = await ti.getFeatures('GK000001.2', 10000, 20000, { scale: 1 })
+    console.log(feats1)
+    expect(feats1).toMatchSnapshot()
   })
 })
