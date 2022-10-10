368d08529ef51970ed9f82a600a82e61ca158481
Colin <colin.diesh@gmail.com>
2022-10-10 09:21:00 -0600

    BigInt polyfill

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
+- Add BigInt64 polyfill for older safari
+
 ## [2.0.2](https://github.com/GMOD/bbi-js/compare/v2.0.1...v2.0.2) (2022-07-18)
-
-
 
 - Make basesCovered a number instead of BigInt
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// from https://github.com/yume-chan/ya-webadb/blob/main/libraries/dataview-bigint-polyfill
+// license:MIT
+// needed for browsers including safari 14
+import './bigint-polyfill/polyfill'
 export { BigWig } from './bigwig'
 export { BigBed } from './bigbed'
 export { Feature, Header, RequestOptions } from './bbi'
